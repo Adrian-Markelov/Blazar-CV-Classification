f6c74df8f6daac19e33ab94228b1b864609b8c3c
Adrian Markelov <amarkelo@andrew.cmu.edu>
2018-03-25 21:07:12 -0400

    Vectorized structure function 1

--- adrian/test_sf.m
+++ adrian/test_sf.m
@@ -1,8 +1,15 @@
 
 
 
-SF = structure_function( 'Blazar_LC.csv', 2 );
+SF_v = structure_function_vect( 'Blazar_LC.csv', 1 );
+%SF = structure_function( 'Blazar_LC.csv', 2 );
 
-size(SF)
 
-scatter( SF(:,1), SF(:,2) );
+%sum(abs(SF-SF_v))
+
+%size(SF)
+%size(SF_v)
+
+%scatter( SF(:,1), SF(:,2), 'b' );
+%hold on
+scatter( SF_v(:,1), SF_v(:,2), 'g*' );
