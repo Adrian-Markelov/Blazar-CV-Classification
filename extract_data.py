c73c1fbdb5d7e1ea8ccc9d045a2a883cb9d09614
Kai Wen Wang <wangkaiwen998@gmail.com>
2018-03-30 22:52:49 -0400

    added extraction code

--- extract_data.py
+++ extract_data.py
@@ -4,17 +4,15 @@ import matplotlib.pyplot as plt
 
 def read_csv(data_source):
     df = pd.read_csv(data_source)
-    mag = dict()
-    date = dict()
-    
-    def add_datapoint(dictionary, point):
-        dictionary[i] = (dictionary.get(i,[])) + [point]
-
-    for (i, m, d) in zip(df['InputID'], df['Mag'], df['MJD']):
-        # print(i, m)
-        add_datapoint(mag, m)
-        add_datapoint(date, d)
-    return (mag, date)
+    a = list(df.groupby('InputID'))
+    result_dict = dict()
+    for dataframe in a:
+        # dataframe[0] contains inputID
+        current_id = dataframe[0]
+        result_dict[current_id] = dict()
+        for k in dataframe[1]:
+            result_dict[current_id][k] = dataframe[1][k]
+    return result_dict
 
 def plot_data(magnitude, date, save_path):
     plt.figure(figsize=(35,40))
@@ -29,9 +27,14 @@ def plot_data(magnitude, date, save_path):
         plt.ylabel('Mag')
     plt.savefig(save_path)
 
-(mag, date) = read_csv('data/Blazar_LC.csv')
-plot_data(mag, date, 'data/Blazar.png')
 
-(mag, date) = read_csv('data/CV_LC.csv')
-plot_data(mag, date, 'data/CV.png')
+if __name__ == '__main__':
+    data = read_csv('data/Blazar_LC.csv')
+    # print(len(mag))
+    # plot_data(mag, date, 'data/Blazar.png')
+
+    # (mag, date) = read_csv('data/CV_LC.csv')
+    # print(len(mag))
+    # plot_data(mag, date, 'data/CV.png')
+
 
