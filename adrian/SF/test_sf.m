9be32122ddb1300ce643ec2ab1cf61f80d6b538d
Adrian Markelov <amarkelo@andrew.cmu.edu>
2018-04-14 11:33:11 -0400

    data analysis stuff

--- adrian/SF/test_sf.m
+++ adrian/SF/test_sf.m
@@ -1,7 +1,21 @@
+fname = 'Blazar_LC.csv'
+fullfile = readtable(fname);
+  
+%Find the unique object IDs
+uniIDs = unique(fullfile.ID, 'stable');
+   
+% Pull out the information associated with object objno
+  
+mjds = fullfile.MJD(fullfile.ID == uniIDs(77));
+mags = fullfile.Mag(fullfile.ID == uniIDs(77));
 
 
+scatter(mjds, mags, '.')
+xlabel('MJDS')
+ylabel('MAG')
+title('Light Curve')
 
-SF_v = structure_function_vect( 'Blazar_LC.csv', 1 );
+%SF_v = structure_function_vect( 'Blazar_LC.csv', 1 );
 %SF = structure_function( 'Blazar_LC.csv', 2 );
 
 
@@ -12,4 +26,4 @@ SF_v = structure_function_vect( 'Blazar_LC.csv', 1 );
 
 %scatter( SF(:,1), SF(:,2), 'b' );
 %hold on
-scatter( SF_v(:,1), SF_v(:,2), 'g*' );
+%scatter( SF_v(:,1), SF_v(:,2), 'g*' );
